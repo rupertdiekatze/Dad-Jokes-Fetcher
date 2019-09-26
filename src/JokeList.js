1bad81e4d06d597b88aca5d753b4214d6476fa0c
Zoltan Szokodi <zoltan.szokodi@hotmail.com>
2019-09-26 12:50:36 +0200

    Fixing Local Storage bug

--- src/JokeList.js
+++ src/JokeList.js
@@ -11,6 +11,7 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = { jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]") };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   async componentDidMount() {
@@ -28,29 +29,38 @@ class JokeList extends Component {
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
 
-    this.setState({
-      jokes
-    });
-    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+    this.setState(st => ({
+      jokes: [...st.jokes, ...jokes]
+    }),
+      () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, delta) {
     this.setState(st => ({
       jokes: st.jokes.map(j => (
         j.id === id ? { ...j, votes: j.votes + delta } : j))
-    }));
+    }),
+      () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick() {
+    this.getJokes();
   }
 
   render() {
     return (
       <div className="JokeList">
+
         <div className="JokeList-sidebar">
           <h1 className="JokeList-title">
             <span>Dad</span> Jokes
           </h1>
           <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>New Jokes</button>
         </div>
+
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
             <Joke
